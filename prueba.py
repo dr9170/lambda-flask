df08fff40c0dcdb43727946e3f58ba583aa5bbf9
Leonardo Noguera <leonardo.noguera.ente@entelgy.com>
2019-03-15 17:56:11 -0500

    adding zappa files

---
+++ prueba.py
@@ -0,0 +1,15 @@
+# app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# here is how we are handling routing with flask:
+@app.route('/')
+def index():
+    return "Hello World! hola javier2000", 200
+
+# include this for local dev
+
+if __name__ == '__main__':
+    app.run()
